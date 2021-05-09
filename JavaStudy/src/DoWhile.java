e88ef1845f51cbe7625f802619d3e78573f93886
liujiao <2943470604@qq.com>
2021-05-09 17:26:16 +0800

    break continue 嵌套

--- JavaStudy/src/DoWhile.java
+++ JavaStudy/src/DoWhile.java
@@ -1,2 +1,10 @@
 public class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            String s = String.format("第%s次输出", i);
+            System.out.println(s);
+            i ++;
+        }while(i <= 10);
+    }
 }
--- JavaStudy/src/For.java
+++ JavaStudy/src/For.java
@@ -1,8 +1,38 @@
+/*
 public class For {
     public static void main(String[] args) {
         for(int i=1; i<=10; i++){
+            if(i==4){
+                break;
+            }
             String s = String.format("第%s次输出", i);
             System.out.println(s);
+        }
+    }
+}
+
+
+
+public class For {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            if(i==4 | i==5){
+                continue;
+            }
+            String s = String.format("第%s次输出", i);
+            System.out.println(s);
+        }
+    }
+}
+*/
+
+// 嵌套循环
+public class For {
+    public static void main(String[] args) {
+        for(int hour=0; hour<24; hour++){
+            for(int minute=0; minute<60; minute++){
+                System.out.println(hour +"时" +minute +"分");
+            }
         }
     }
 }
